05458e257991c6fadd68a309c63951384e2aed4e
Larry Hsiao <larryhsiao@silverhetch.com>
2018-08-29 13:12:30 +0800

    apply sonarqube plugins

---
+++ src/test/kotlin/com/silverhetch/carpo/edge/SimpleEdgeTest.kt
@@ -0,0 +1,11 @@
+package com.silverhetch.carpo.edge
+
+import org.junit.Assert
+import org.junit.Test
+
+class SimpleEdgeTest {
+    @Test
+    fun simple() {
+        Assert.assertTrue(true)
+    }
+}
