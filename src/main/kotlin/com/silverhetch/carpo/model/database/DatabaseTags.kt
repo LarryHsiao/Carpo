e3b03290d41866e755c169a40c57d110c82ee75a
Larry Hsiao <larryhsiao@silverhetch.com>
2018-09-05 10:53:46 +0800

    Use expression for createStatement on Connection of SQL

--- src/main/kotlin/com/silverhetch/carpo/model/database/DatabaseTags.kt
+++ src/main/kotlin/com/silverhetch/carpo/model/database/DatabaseTags.kt
@@ -5,19 +5,21 @@ import java.sql.Connection
 
 class DatabaseTags(private val db: Source<Connection>, private val fileName: String) : Source<List<String>> {
     override fun fetch(): List<String> {
-        db.fetch().createStatement().executeQuery(
-            """
+        db.fetch().createStatement().use { statement ->
+            statement.executeQuery(
+                """
                 select tags.name
                 from tags
                 join files as file, file_tag
                 where file.name = '$fileName' and tags.id = file_tag.tag_id and file.id = file_tag.file_id;
                   """
-        ).use {
-            with(ArrayList<String>()) {
-                while (it.next()) {
-                    add(it.getString(it.findColumn("name")))
+            ).use {
+                with(ArrayList<String>()) {
+                    while (it.next()) {
+                        add(it.getString(it.findColumn("name")))
+                    }
+                    return this
                 }
-                return this
             }
         }
     }
