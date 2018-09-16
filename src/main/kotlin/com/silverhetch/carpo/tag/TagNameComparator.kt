34f3ee5222d743062e5936ab8992c8e10a308ec4
Larry Hsiao <larryhsiao@silverhetch.com>
2018-09-16 17:06:29 +0800

    Comment for Name comparator

--- src/main/kotlin/com/silverhetch/carpo/tag/TagNameComparator.kt
+++ src/main/kotlin/com/silverhetch/carpo/tag/TagNameComparator.kt
@@ -1,7 +1,10 @@
 package com.silverhetch.carpo.tag
 
-import java.util.Comparator
+import java.util.*
 
+/**
+ * Comparator decorator which compares [Tag] in Name.
+ */
 class TagNameComparator(private val comparator: Comparator<String>) : Comparator<Tag> {
     override fun compare(o1: Tag, o2: Tag): Int {
         return comparator.compare(o1.title(), o2.title())
