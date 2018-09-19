612985887b85912ff791ac8893cdeaf4ba51b22e
Larry Hsiao <larryhsiao@silverhetch.com>
2018-09-19 17:57:40 +0800

    Tag management view

---
+++ src/main/kotlin/com/silverhetch/carpo/javafx/TagManagementView.kt
@@ -0,0 +1,26 @@
+package com.silverhetch.carpo.javafx
+
+import com.jfoenix.controls.JFXTextField
+import com.silverhetch.carpo.tag.Tags
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class TagManagementView : Initializable {
+    @FXML private lateinit var tagListController: TagListView
+    @FXML private lateinit var searchField: JFXTextField
+
+    private lateinit var tags: Tags
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        searchField.textProperty().addListener { _, _, newValue ->
+            tagListController.loadTags(tags.byName(newValue))
+        }
+    }
+
+    fun loadTags(tags: Tags) {
+        this.tags = tags
+        tagListController.loadTags(tags.all())
+    }
+}
