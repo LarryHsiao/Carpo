3dc5df40109c458845366fd377e50873d9df5001
LarryHsiao <larryhsiao@silverhetch.com>
2018-10-03 00:49:26 +0800

    JdkPropertiesConfig store new value back to file

--- src/test/kotlin/com/silverhetch/carpo/config/CarpoConfigSourceTest.kt
+++ src/test/kotlin/com/silverhetch/carpo/config/CarpoConfigSourceTest.kt
@@ -2,6 +2,7 @@ package com.silverhetch.carpo.config
 
 import org.junit.Assert
 import org.junit.Test
+import java.io.File
 import java.io.FileWriter
 import java.io.PrintWriter
 import java.nio.file.Files
@@ -26,6 +27,51 @@ class CarpoConfigSourceTest {
         }
         Assert.assertEquals(
             "/home/abc/def",
+            CarpoConfigSource(tempConfig.absolutePath).fetch().workspacePath()
+        )
+    }
+
+    @Test
+    fun newWorkspace_notExist() {
+        val tempConfig = Files.createTempFile("", "").toFile().also {
+            val writer = PrintWriter(FileWriter(it, false))
+            writer.printf("workspace=/home/abc/def")
+            writer.close()
+        }
+        val config = CarpoConfigSource(tempConfig.absolutePath).fetch()
+        config.changeWorkspacePath(File("/newFileLocation"))
+        Assert.assertEquals(
+            "/home/abc/def",
+            config.workspacePath()
+        )
+    }
+
+    @Test
+    fun newWorkspace_exist() {
+        val tempConfig = Files.createTempFile("", "").toFile().also {
+            val writer = PrintWriter(FileWriter(it, false))
+            writer.printf("workspace=/home/abc/def")
+            writer.close()
+        }
+        val newWorkspace = Files.createTempDirectory("").toFile()
+        val config = CarpoConfigSource(tempConfig.absolutePath).fetch()
+        config.changeWorkspacePath(newWorkspace)
+        Assert.assertEquals(
+            newWorkspace.absolutePath,
+            config.workspacePath()
+        )
+    }
+
+    @Test
+    fun nonExistConfigFile_ableChangeWorkspace() {
+        val tempConfig = Files.createTempFile("", "").toFile().also {
+            it.deleteRecursively()
+        }
+        val newWorkspace = Files.createTempDirectory("").toFile()
+        val config = CarpoConfigSource(tempConfig.absolutePath).fetch()
+        config.changeWorkspacePath(newWorkspace)
+        Assert.assertEquals(
+            newWorkspace.absolutePath,
             CarpoConfigSource(tempConfig.absolutePath).fetch().workspacePath()
         )
     }
