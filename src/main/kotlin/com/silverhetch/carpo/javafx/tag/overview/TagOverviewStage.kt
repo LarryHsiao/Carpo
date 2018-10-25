f68ea6939e6b94410e50e47587f5d41bdd045170
LarryHsiao <bleakeraser1997@gmail.com>
2018-10-25 23:28:11 +0800

    Introduce JFXDecoratorParent.kt

--- src/main/kotlin/com/silverhetch/carpo/javafx/tag/overview/TagOverviewStage.kt
+++ src/main/kotlin/com/silverhetch/carpo/javafx/tag/overview/TagOverviewStage.kt
@@ -1,11 +1,10 @@
 package com.silverhetch.carpo.javafx.tag.overview
 
-import com.jfoenix.controls.JFXDecorator
 import com.silverhetch.carpo.javafx.Stylesheets
+import com.silverhetch.carpo.javafx.utility.jfoenix.JFXDecoratorParent
 import com.silverhetch.carpo.tag.Tag
 import com.silverhetch.clotho.Source
 import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 import java.util.*
@@ -15,11 +14,7 @@ class TagOverviewStage(private val resources: ResourceBundle, private val select
         stage.scene = Scene(
             FXMLLoader(javaClass.getResource("/TagOverview.fxml")).let { loader ->
                 loader.resources = resources
-                val view = if (stage.isShowing) {
-                    loader.load<Parent>()
-                } else {
-                    JFXDecorator(stage, loader.load<Parent>())
-                }
+                val view = JFXDecoratorParent(stage, loader.load()).fetch()
                 view.id = "TagOverviewWindow"
                 loader.getController<TagOverviewView>().loadTag(selected)
                 view
